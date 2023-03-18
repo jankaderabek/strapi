7856c8f6e5a4becdd1ce79687f141c85ef798983
jankaderabek <kaderabek.jan@gmail.com>
2023-03-18 18:45:59 +0100

    don't use team filter

--- config/plugins.js
+++ config/plugins.js
@@ -23,8 +23,7 @@ module.exports = ({ env }) => ({
       config: {
         deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
         apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-        appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-        teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+        appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER
       },
     },
   });
